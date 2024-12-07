c938391970cbf46e4d7b142637f0ac6f62e19d39
paq1 <p.paquin1605@gmail.com>
2024-12-07 10:55:40 +0100

    refactor

---
+++ src/core/queue/sync/queue_synchronizer.rs
@@ -0,0 +1,45 @@
+use crate::core::queue::can_produce::CanProduceInQueue;
+use crate::core::queue::datas::Data;
+use crate::core::queue::sync::subscriber::CanSubscribe;
+use async_trait::async_trait;
+use serde::Serialize;
+use std::fmt::Debug;
+use std::sync::mpsc::channel;
+use std::sync::Arc;
+use std::time::Duration;
+
+#[async_trait]
+pub trait QueueSynchronizer<M, R>: Send + Sync
+where
+    R: Send,
+{
+    async fn wait_result(&self, correlation_id: &str, topic: &str, message: &M, key: Option<&str>) -> Result<R, String>;
+}
+
+pub struct QueueSynchronizerImpl<M, R> {
+    producer: Arc<dyn CanProduceInQueue<M>>,
+    subscriber: Arc<dyn CanSubscribe<R>>,
+}
+
+#[async_trait]
+impl<M, R> QueueSynchronizer<M, R> for QueueSynchronizerImpl<M, R>
+where
+    R: Send + Debug,
+    M: Clone + Debug + Send + Sync + Serialize,
+{
+    async fn wait_result(&self, correlation_id: &str, topic: &str, message: &M, key: Option<&str>) -> Result<R, String> {
+        let (tx, rx) = channel();
+        self.subscriber.subscribe(correlation_id, tx).await?;
+        self.producer.produce_data(topic, &Data {data: message.clone()}, key)?;
+
+        let c = rx
+            .recv_timeout(Duration::from_secs(30))
+            .map(|msg| {
+                println!("Reçu : {msg:?}");
+                msg
+            })
+            .map_err(|e| format!("Erreur lors de l'envoi: {}", e))?;
+
+        self.subscriber.unsubscribe(correlation_id).await.map(|_| c)
+    }
+}
---
+++ src/core/queue/sync/subscriber.rs
@@ -0,0 +1,63 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::mpsc::Sender;
+use async_trait::async_trait;
+use futures::lock::Mutex;
+
+#[async_trait]
+pub trait CanSubscribe<RESPONSE>: Send + Sync
+where
+    RESPONSE: Send,
+{
+    async fn subscribe(&self, correlation_id: &str, sender: Sender<RESPONSE>)
+                       -> Result<(), String>;
+    async fn unsubscribe(&self, correlation_id: &str) -> Result<(), String>;
+    async fn send(&self, correlation_id: &str, message: RESPONSE) -> Result<(), String>;
+}
+
+pub struct Subscriber<RESPONSE>
+where
+    RESPONSE: Send,
+{
+    pub datas: Arc<Mutex<HashMap<String, Sender<RESPONSE>>>>,
+}
+
+
+#[async_trait]
+impl<RESPONSE> CanSubscribe<RESPONSE> for Subscriber<RESPONSE>
+where
+    RESPONSE: Send,
+{
+    async fn subscribe(
+        &self,
+        correlation_id: &str,
+        sender: Sender<RESPONSE>,
+    ) -> Result<(), String> {
+        let mut lock = self.datas.lock().await;
+        // Stocke un Arc<Sender> directement
+        lock.insert(correlation_id.to_string(), sender);
+        Ok(())
+    }
+
+    async fn unsubscribe(&self, correlation_id: &str) -> Result<(), String> {
+        let mut lock = self.datas.lock().await;
+        // Stocke un Arc<Sender> directement
+        lock.remove(correlation_id);
+        Ok(())
+    }
+
+    async fn send(&self, correlation_id: &str, message: RESPONSE) -> Result<(), String> {
+        let lock = self.datas.lock().await;
+
+        if let Some(sender_arc) = lock.get(correlation_id) {
+            // Cloner l'Arc<Sender> pour envoyer le message
+            sender_arc
+                .send(message)
+                .map_err(|e| e.to_string())?;
+        } else {
+            return Err(format!("Aucun Sender trouvé pour l'ID '{}'", correlation_id).into());
+        }
+
+        Ok(())
+    }
+}
